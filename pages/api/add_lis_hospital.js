6f3ed399d58f9bb0a125c38cbbfc53e424938daa
Hakim Mudor <78400565+kimookpong@users.noreply.github.com>
2023-02-07 19:07:25 +0700

    update

--- pages/api/add_lis_hospital.js
+++ pages/api/add_lis_hospital.js
@@ -1,26 +1,36 @@
 import dbconnect from "./dbconnect";
 const connection = dbconnect();
 
-connection.connect(function(err) {
-    if (err) {
-      console.error('Error connecting to database: ' + err.stack);
-      return;
-    }
-    console.log('Connected to database as id ' + connection.threadId);
+connection.connect(function (err) {
+  if (err) {
+    console.error("Error connecting to database: " + err.stack);
+    return;
+  }
+  console.log("Connected to database as id " + connection.threadId);
 });
 
-export default function handler(req,res){
-    const name = req.body.name;
-    const nameeng = req.body.nameeng;
-    const address = req.body.address;
-    const tel = req.body.tel;
-    const dept = req.body.dept;
+export default function handler(req, res) {
+  const name = req.body.name;
+  const nameeng = req.body.nameeng;
+  const address = req.body.address;
+  const tel = req.body.tel;
+  const dept = req.body.dept;
 
-    const query = `INSERT INTO lis_hospital (hospital_name_th, hospital_name_en, address, phone, hospital_department_lab) VALUES ('${name}', '${nameeng}', '${address}', '${tel}', '${dept}')`;
-    connection.query(query, function(err, result) {
-        if (err) {
-        console.error(err);
-        return;
-        }
-    });
+  console.log(req.body);
+
+  const query = `UPDATE lis_hospital SET 
+  hospital_name_th = '${name}', 
+  hospital_name_en = '${nameeng}', 
+  address = '${address}', 
+  phone = '${tel}',
+  hospital_department_lab = '${dept}'
+  WHERE id_hospital = '1'`;
+  connection.query(query, function (err, result) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    res.status(200).json({ status: true });
+  });
 }
--- pages/api/get_lis_hospital.js
+++ pages/api/get_lis_hospital.js
@@ -1,27 +1,21 @@
 import dbconnect from "./dbconnect";
 const connection = dbconnect();
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
-    console.error('Error connecting to database: ' + err.stack);
+    console.error("Error connecting to database: " + err.stack);
     return;
   }
-  console.log('Connected to database as id ' + connection.threadId);
+  console.log("Connected to database as id " + connection.threadId);
 });
 
 export default function handler(req, res) {
-  const name = req.body.name;
-  const nameeng = req.body.nameeng;
-  const address = req.body.address;
-  const tel = req.body.tel;
-  const dept = req.body.dept;
-
   const query = `SELECT hospital_name_th, hospital_name_en, address, phone, hospital_department_lab FROM lis_hospital WHERE id_hospital = '1'`;
-  connection.query(query, function(err, rows, fields) {
+  connection.query(query, function (err, rows, fields) {
     if (err) {
       console.error(err);
       return;
     }
-    res.status(200).json(rows)
+    res.status(200).json(rows);
   });
 }
