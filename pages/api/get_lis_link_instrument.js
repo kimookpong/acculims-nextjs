429021b8a26251e4ece31a5d79ccdabf746a0fb1
Hakim Mudor <78400565+kimookpong@users.noreply.github.com>
2023-02-28 20:13:48 +0700

    Update get_lis_link_instrument.js

--- pages/api/get_lis_link_instrument.js
+++ pages/api/get_lis_link_instrument.js
@@ -1,21 +1,21 @@
 import dbconnect from "./dbconnect";
 const connection = dbconnect();
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
-    console.error('Error connecting to database: ' + err.stack);
+    console.error("Error connecting to database: " + err.stack);
     return;
   }
-  console.log('Connected to database as id ' + connection.threadId);
+  console.log("Connected to database as id " + connection.threadId);
 });
 
 export default function handler(req, res) {
-  const query = 'SELECT * FROM `lis_link_instrument`';
-  connection.query(query, function(err, rows, fields) {
+  const query = "SELECT * FROM lis_link_instrument";
+  connection.query(query, function (err, rows, fields) {
     if (err) {
       console.error(err);
       return;
     }
-    res.status(200).json(rows)
+    res.status(200).json(rows);
   });
 }
