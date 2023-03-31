eff08987d502f9f0a1b0593f7254891123153843
Pongphan <Pongphan@users.noreply.github.com>
2023-03-31 14:08:07 +0700

    Create add_lab_order_form.js

---
+++ pages/api/add_lab_order_form.js
@@ -0,0 +1,42 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
+});
+
+connection.connect(function(err) {
+    if (err) {
+      console.error('Error connecting to database: ' + err.stack);
+      return;
+    }
+    console.log('Connected to database as id ' + connection.threadId);
+  });
+
+export default function handler(req, res) {
+  const labOrder = req.body.labOrder;
+  
+  //data header
+  const hn = req.body.hn;
+  const name = req.body.name;
+  const sex = req.body.sex;
+  const age = req.body.age;
+  //labdetail
+  const test = req.body.test;
+  const payment = req.body.payment;
+  const cost = req.body.cost;
+  const invoice = req.body.invoice;
+
+  const query = `INSERT INTO lab_order_form (hn,name,sex,age,test,payment,cost,invoice) 
+  VALUES ('${hn}','${name}','${sex}','${age}','${test}','${payment}','${cost}','${invoice})`;
+  
+  connection.query(query, function(err, result) {
+      if (err) {
+      console.error(err);
+      return;
+      }
+      console.log(result);
+  });
+}
